8a351e2a8f8bc7d41ec8026920f370c2697d21fe
jiazhiruan <91059363+jiazhiruan@users.noreply.github.com>
2021-10-03 11:55:30 +0100

    altered by Russell

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,19 @@
 # Task 1: Generalised Fibonacci
+p = 1
+q = 2
+fibo =[1,1]
+
+#fibo.append(fibo[0]+fibo[-1])
+#print(fibo)
+#fibo.append(fibo[0]+fibo[1]+fibo[2])
+#print(fibo)
+
 
 # Write your code here:
 
+n=4
+for i in range (2,n):
+    fibo.append(fibo[i-2]+fibo[i-1])
+
+print(fibo)
 
